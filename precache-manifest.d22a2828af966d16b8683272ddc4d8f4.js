f74e484a91b95bf329ef8fed6788006fb0c0f1c0
narojuvamshidhar <narojuvamshidhar@gmail.com>
2020-04-23 02:14:54 +0530

    Updates

---
+++ precache-manifest.d22a2828af966d16b8683272ddc4d8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409b314285484bc62188fe6db902f0fe",
+    "url": "/robojs/index.html"
+  },
+  {
+    "revision": "c04fc76d47109a87de45",
+    "url": "/robojs/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f6ae4bf29b62d78e4521",
+    "url": "/robojs/static/css/main.a33bd3fe.chunk.css"
+  },
+  {
+    "revision": "c04fc76d47109a87de45",
+    "url": "/robojs/static/js/2.2f5fa15f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robojs/static/js/2.2f5fa15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ae4bf29b62d78e4521",
+    "url": "/robojs/static/js/main.9e2b821b.chunk.js"
+  },
+  {
+    "revision": "28c3584455af6befed04",
+    "url": "/robojs/static/js/runtime-main.9205b862.js"
+  }
+]);
